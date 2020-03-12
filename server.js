d9ea2b90b57e128ec7fb7d935e02ac466d42cb7d
Jose Ortiz <joseortiz@imac-de-jose.lan>
2020-03-12 11:05:06 -0400

    corregido error de sintaxis que no permitia obtener la ruta una vez se actualizara la pagina en una ruta distinta a la home (/)

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Handles any requests that don't match the ones above
-app.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname+'build/index.html'));
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
 const port = process.env.PORT || 6001;

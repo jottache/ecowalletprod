f9f7d0a9f5726f9212a6f62b75bb2608f8e506ef
jose ortiz <jottache20@gmail.com>
2020-03-20 13:34:04 -0400

    añadida vista de recovery pass y actualizacion de build

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'build/index.html'));
+    res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
 const port = process.env.PORT || 6001;
